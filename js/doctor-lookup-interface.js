8e7ea5f9997014c4df1adf9d912a19dc3803f828
Philip Putnam <putnamp989@gmail.com>
2017-03-17 12:29:32 -0700

    add doctor profile pictures, name link decorations, and overflow styling to doctor-info div

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -5,10 +5,8 @@ var displaySearchResults = function(searchResults) {
   $('.col-md-3').show();
   $('#search-results').append("<h2>Search Results: </h2><h2>Doctors:</h2>");
   var doctors = searchResults.results[0].data;
-
   for (var index = 0; index < doctors.length; index++) {
     $('#search-results').append("<p class='doctor'><strong>" + doctors[index].profile.first_name + ' ' + doctors[index].profile.last_name + "</strong></p>");
-
     addDoctorInfo(doctors, index);
   }
 };
@@ -17,20 +15,16 @@ function addDoctorInfo(doctors, index) {
   $('p.doctor').last().click(function() {
     $('.col-md-8').show();
     $('#doctor-info').text('');
-    $('#doctor-info').append("<p><strong>" + doctors[index].profile.first_name + ' ' + doctors[index].profile.last_name + "</strong></p><p><p><strong>Bio:</strong></p><p>" +  doctors[index].profile.bio +
+    $('#doctor-info').append("<p><strong>" + doctors[index].profile.first_name + ' ' + doctors[index].profile.last_name + "<br><img src='" + doctors[index].profile.image_url + "'></strong></p><p><p><strong>Bio:</strong></p><p>" +  doctors[index].profile.bio +
     "</p></div>");
   });
 }
 
 $(document).ready(function() {
-
   $('#doctor-search').click(function(event) {
     event.preventDefault();
-
     var medicalIssue = $('#medical-issue').val();
     var search = new Search();
     search.getDoctors(medicalIssue, displaySearchResults);
-
   });
-
 });
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -8,7 +8,7 @@ function Search() {
 
 Search.prototype.getDoctors = function(medicalIssue, displayResults) {
   var search = this;
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query= '+ medicalIssue + '&location=45.5231%2C-122.413%2C100&user_location=45.5231%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query= '+ medicalIssue + '&location=45.5231%2C-122.413%2C100&user_location=45.5231%2C-122.413&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
     search.results.push(result);
     displayResults(search);
