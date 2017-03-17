d26f161941a7a3630fa289026f86b40b46a9ab48
Philip Putnam <putnamp989@gmail.com>
2017-03-17 09:44:51 -0700

    add hashtag to medicalIssue jQuery in -interface to fix bug

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -1,13 +1,17 @@
 var Search = require('./../js/doctor-lookup.js').searchModule;
 
+var displaySearchResults = function(result) {
+  console.log(result);
+};
+
 $(document).ready(function() {
 
   $('#doctor-search').click(function(event) {
     event.preventDefault();
 
-    var medicalIssue = $('medical-issue').val();
+    var medicalIssue = $('#medical-issue').val();
     var search = new Search();
-    search.getDoctors(medicalIssue);
+    search.getDoctors(medicalIssue, displaySearchResults);
 
   });
 
--- js/doctor-lookup.js
+++ js/doctor-lookup.js
@@ -2,14 +2,14 @@ var apiKey = require('./../.env').apiKey;
 
 
 function Search() {
-  this.searches = [];
+
 }
 
 
-Search.prototype.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query='+ medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
+Search.prototype.getDoctors = function(medicalIssue, displayResults) {
+  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query= '+ medicalIssue + '&location=45.5231%2C-122.413%2C100&user_location=45.5231%2C-122.413&skip=0&limit=10&user_key=' + apiKey)
    .then(function(result) {
-      console.log(result);
+      displayResults(result);
     })
    .fail(function(error){
       console.log("fail");
