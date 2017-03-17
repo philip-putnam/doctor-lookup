6829490506e0d9967f1e1d986b4521fe664c7b5a
Philip Putnam <putnamp989@gmail.com>
2017-03-17 12:05:04 -0700

    add bio div and styling

--- js/doctor-lookup-interface.js
+++ js/doctor-lookup-interface.js
@@ -2,18 +2,25 @@ var Search = require('./../js/doctor-lookup.js').searchModule;
 
 var displaySearchResults = function(searchResults) {
   $('#search-results').text('');
+  $('.col-md-3').show();
   $('#search-results').append("<h2>Search Results: </h2><h2>Doctors:</h2>");
   var doctors = searchResults.results[0].data;
 
   for (var index = 0; index < doctors.length; index++) {
-    $('#search-results').append("<p class='doctor'><strong>" + doctors[index].profile.first_name + ' ' + doctors[index].profile.last_name +
-                                "</strong></p><div class='initially-hidden' id='" + index + "'><p><strong>Bio:</strong></p><p>" +  doctors[index].profile.bio +
-                                "</p></div>");
-    $('p.doctor').last().click(function() {
-      $('div#' + this.nextSibling.id).toggle();
-    });
+    $('#search-results').append("<p class='doctor'><strong>" + doctors[index].profile.first_name + ' ' + doctors[index].profile.last_name + "</strong></p>");
+
+    addDoctorInfo(doctors, index);
   }
 };
+
+function addDoctorInfo(doctors, index) {
+  $('p.doctor').last().click(function() {
+    $('.col-md-8').show();
+    $('#doctor-info').text('');
+    $('#doctor-info').append("<p><strong>" + doctors[index].profile.first_name + ' ' + doctors[index].profile.last_name + "</strong></p><p><p><strong>Bio:</strong></p><p>" +  doctors[index].profile.bio +
+    "</p></div>");
+  });
+}
 
 $(document).ready(function() {
 
