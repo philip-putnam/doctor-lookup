5d579601db7c0b2e6221d767c0c09f082054ea0c
Philip Putnam <putnamp989@gmail.com>
2017-03-17 08:50:42 -0700

    add index.html, doctor-lookup.js, and doctor-lookup-interface.js content with simple form

---
+++ js/doctor-lookup-interface.js
@@ -0,0 +1,11 @@
+var Search = require('./../js/doctor-lookup.js').searchModule;
+
+$(document).ready(function() {
+
+  $('#search-results').click(function(event) {
+    event.preventDefault();
+
+
+  });
+
+});
